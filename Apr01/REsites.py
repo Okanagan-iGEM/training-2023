125b0e2575d4bdc33e7a88f452085e49bdc24724
Sharan S <sharansr@pm.me>
2023-03-31 21:52:54 -0700

    Added REsites

---
+++ Apr01/REsites.py
@@ -0,0 +1,12 @@
+# Only the sense strands are listed
+sites = {
+    "BamHI": "GGATCC",
+    "ClaI": "ATCGAT",
+    "EcoRI": "GAATTC",
+    "EcoRV": "GATATC",
+    "HaeIII": "GGCC",
+    "HindIII": "AAGCTT",
+    "NotI": "GCGGCCGC",
+    "PstI": "CTGCAG",
+    "PvuII": "CAGCTG"
+}
