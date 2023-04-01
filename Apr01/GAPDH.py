090069ca2f37eb4f46329a908129b57f8c1d9a28
Sharan S <sharansr@pm.me>
2023-03-31 19:55:28 -0700

    Added Apr01 stuff

---
+++ Apr01/GAPDH.py
@@ -0,0 +1,4 @@
+# Load the sequence for GAPDH
+GAPDH_seq = open("files/Homo_sapiens_GAPDH.txt", "r").read()
+
+# Write your code here
---
+++ Apr01/rRNA.py
@@ -0,0 +1,5 @@
+# Load the sequences
+Pyrococcus_rRNA = open("files/Pyrococcus_rRNA.txt", "r").read()
+Ecoli_rRNA = open("files/Ecoli_rRNA.txt", "r").read()
+
+# Write your code here
